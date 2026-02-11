b44cb6a7b963337aef6ffb1fea75a621949b0b2c
Joao Pedro <joaopedroabc201@gmail.com>
2026-02-11 16:39:51 -0300

    fix: force update database config

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,25 +1,23 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConfigModule } from '@nestjs/config'; // Importante para ler variáveis
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
 
 @Module({
   imports: [
-    // 1. Carrega as variáveis de ambiente (do .env ou do Render)
     ConfigModule.forRoot({
       isGlobal: true,
     }),
 
-    // 2. Configura o Banco de Dados usando a URL
     TypeOrmModule.forRoot({
       type: 'postgres',
-      url: process.env.DATABASE_URL, // Pega a URL do Neon
+      url: process.env.DATABASE_URL,
       autoLoadEntities: true,
-      synchronize: true, // ⚠️ Em produção real, usariamos Migrations, mas para MVP ok.
+      synchronize: true,
       ssl: {
-        rejectUnauthorized: false, // Necessário para conectar no Neon/Render com segurança
+        rejectUnauthorized: false,
       },
     }),
 
