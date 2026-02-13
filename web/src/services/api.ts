c2074968003d44b85cd1b46187aa3fab8a35797d
Joao Pedro <joaopedroabc201@gmail.com>
2026-02-12 22:18:25 -0300

    ss

--- web/src/services/api.ts
+++ web/src/services/api.ts
@@ -1,7 +1,11 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'https://drop-manager.onrender.com',
+  // COMENTE A LINHA DO IMPORT.META:
+  // baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3000',
+  
+  // COLE O LINK DIRETO AQUI:
+  baseURL: 'https://drop-manager-api.onrender.com',
 });
 
 export default api;
